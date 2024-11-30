99eb0d182fc38b2efd10b99ca232291f7147f2d2
Ignacia Silva <ignaciasilva1@gmail.com>
2024-11-30 12:57:29 -0300

    show entered expenses along with their css styles

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref, reactive } from 'vue';
 import Presupuesto from './components/Presupuesto.vue';
 import ControlPresupuesto from './components/ControlPresupuesto.vue';
 import Modal from './components/Modal.vue'
+import Gasto from './components/Gasto.vue'
 import {generarId} from './helpers/index'
 import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
@@ -57,12 +58,12 @@ const guardarGasto = ()=>{
     id: null,
     fecha: Date.now()
     })
-
 }
 </script>
 
 <template>
-<div>
+<div
+:class="{fijar:modal.mostrar}">
     <header>
        <h1> Planificador de gastos</h1>
        <div class="contenedor-header contenedor sombra">
@@ -79,6 +80,17 @@ const guardarGasto = ()=>{
     </header>
 
     <main v-if="presupuesto>0">
+
+        <div class="listado-gastos contenedor">
+            <h2>{{ gastos.length >0 ? 'Gastos': 'No hay gastos' }}</h2>
+            <Gasto
+            v-for="gasto in gastos"
+            :key="gasto.id"
+            :gasto ="gasto"
+            />
+        </div>
+
+
 <div class="crear-gasto">
 <img :src="iconoNuevoGasto" alt="icono nuevo gasto"
 @click="mostrarModal"
@@ -134,6 +146,10 @@ h1{
 h2{
     font-size: 3rem;
 }
+.fijar{
+    overflow: hidden;
+    height: 100vh;
+}
 header{
     background-color: var(--azul);
 }
@@ -169,6 +185,15 @@ right: 5rem;
 .crear-gasto img{
 width: 5rem;
 cursor: pointer;
+}
+
+.listado-gastos{
+margin-top: 10rem;
+}
+
+.listado-gastos h2{
+font-weight: 900;
+color: var(--gris-oscuro);
 }
 
 </style>
